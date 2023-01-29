7b3d375756f67c388df6dce6afb76487419cf697
alfredsandare <alfred.s.sandare04@gmail.com>
2023-01-29 23:53:14 +0100

    a

--- main.py
+++ main.py
@@ -47,7 +47,8 @@ class Game:
         'game_running': The game is running. It is always the players move, as the computer makes its move instantly'''
         self.state = 'player_setup'
         self.update_status_text()
-        self.temporary_ship = None  # used for hovering while placing ships
+        #self.temporary_ship = None  # used for hovering while placing ships
+        self.temporary_ship_anchor = None
 
     def update_status_text(self):
         if self.state == 'player_setup':
@@ -69,8 +70,9 @@ class Game:
             '''
             if grid1.check_clear_space_for_ship(True, 3, mouse_grid_pos):
                 #self.temporary_ship = Ship(True, 3, mouse_grid_pos)
-                grid1.delete_ship(mouse_grid_pos)
-                grid1.add_ship(True, 3, mouse_grid_pos)
+                grid1.delete_ship(self.temporary_ship_anchor)
+                self.temporary_ship_anchor = mouse_grid_pos
+                grid1.add_ship(True, 3, self.temporary_ship_anchor)
             grid1.update_grid()
 
 
@@ -110,8 +112,6 @@ class Grid:
             self.update_grid()
 
     def check_clear_space_for_ship(self, direction, length, pos):
-        #print('andra')
-        #self.print_grid()
         success = False
         x, y = get_2d_coords(pos)
         if direction:  # horizontal
@@ -120,7 +120,6 @@ class Grid:
         else:  # vertical
             if y + length < 9 and all([self.grid[pos + 10 * j] == 0 for j in range(length)]):
                 success = True
-        #print(success, x + length < 11, x, length)
         return success
 
     def add_ship(self, direction, length, pos):
@@ -140,8 +139,8 @@ class Grid:
                 break
         if ship_id is not None:
             self.ships.pop(ship_id)
-            print(pos, self.tk_ships)
-            del self.tk_ships[pos]
+            self.canvas.delete(self.tk_ships[pos])  # delete the sprite
+            del self.tk_ships[pos]  # delete the variable
         return ship_id is not None
 
     def update_grid(self):
@@ -151,50 +150,10 @@ class Grid:
         for i in range(len(self.grid)):
             self.grid[i] = 0
 
-        '''
-        grid_data = [2, 1]
         for ship in self.ships:
             for i, cell in enumerate(ship.cells):
-                if ship.direction:  # direction is horizontal
-                    self.grid[ship.anchor+i] = grid_data[cell]
-                else:  # direction is vertical
-                    self.grid[ship.anchor+10*i] = grid_data[cell]
-        '''
-
-
-        #for ship in self.ships:
-        #    for i, cell in enumerate(ship.cells):
-
-
-
-
-        '''
-        if game.temporary_ship is not None:  # if there is a temporary ship
-            for i in range(len(game.temporary_ship.cells)):
-                if game.temporary_ship.direction:  # direction is horizontal
-                    self.grid[game.temporary_ship.anchor+i] = 1
-                else:  # direction is vertical
-                    self.grid[game.temporary_ship.anchor+10*i] = 1
-        '''
-
-        # update the tk_grid
-        if self.view:
-            '''
-            for i, cell in enumerate(self.grid):
-                self.canvas.itemconfig(self.canvas_grid[i], image=frame.images[game.cellImages[cell]])
-
-            if game.temporary_ship is not None:
-                for i in range(len(game.temporary_ship.cells)):
-                    if game.temporary_ship.direction:  # direction is horizontal
-                        self.canvas.itemconfig(self.canvas_grid[game.temporary_ship.anchor+i], image=frame.images[game.cellImages[1]])
-                    else:  # direction is vertical
-                        self.canvas.itemconfig(self.canvas_grid[game.temporary_ship.anchor+10*i], image=frame.images[game.cellImages[1]])
-            '''
-
-            #for ship in self.ships:
-            #    if len(ship.cells) == 3:
-            pass
-
+                grid_pos = ship.anchor+i if ship.direction else ship.anchor+10*i
+                self.grid[grid_pos] = 1 if cell else 2
 
     def print_grid(self):
         ''' Prints the grid to the console. '''
